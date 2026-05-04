908997dc8513422a955630d04cd995413c6aac3b
Jéssica Pasqualini <jessicadaspasqualini@gmail.com>
2026-05-04 16:51:02 -0300

    feat: adiciona exercício Números Negativos

--- src/vetores/NumerosNegativos.java
+++ src/vetores/NumerosNegativos.java
@@ -1,4 +1,33 @@
 package vetores;
 
+
+/**
+ * Faça um programa que leia N números inteiros e armazene-os
+ * em um vetor. Em seguida, mostrar na tela todos os números
+ * negativos lidos.
+ */
+
+import java.util.Scanner;
+
 public class NumerosNegativos {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        int N;
+
+        N = input.nextInt();
+
+        int [] vetor = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            vetor[i] = input.nextInt();
+        }
+        for (int i = 0; i < N; i++) {
+            if (vetor[i] < 0) {
+                System.out.println(vetor[i]);
+            }
+        }
+        input.close();
+    }
 }
