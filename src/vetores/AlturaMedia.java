2307e549b72741390179f2f5b69c8a6b1d1846c0
Jéssica Pasqualini <jessicadaspasqualini@gmail.com>
2026-05-05 17:39:37 -0300

    feat: adiciona exercício Altura Média

--- src/vetores/AlturaMedia.java
+++ src/vetores/AlturaMedia.java
@@ -1,3 +1,12 @@
+package vetores;
+
+/**
+ * Fazer um programa para ler um número N, depois nome (apenas uma
+ * palavra sem espaços), idade e altura de N pessoas, conforme exemplo.
+ * Depois, mostrar na tela a altura média das pessoas, e mostrar também a
+ * porcentagem de pessoas com menos de 16 anos.
+ */
+
 import java.util.Locale;
 import java.util.Scanner;
 
